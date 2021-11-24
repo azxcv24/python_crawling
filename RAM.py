0840f8a01502c119719d643b5ecbb3924a2b5a60
azxcv24 <azxcv24@gmail.com>
2021-11-25 02:07:15 +0900

    추가

--- RAM.py
+++ RAM.py
@@ -115,11 +115,17 @@ while curPage <= totalPage:
         #상품명
         name = v.select_one('p.prod_name > a').text.strip()
         #가격(정수로 변환,반올림으로 정가 추가)
-        price1 = v.select_one('li.rank_one > p > a > strong').text.strip()
+        try:
+            price1 = v.select_one('li.rank_one > p > a > strong').text.strip()
+        except Exception as e:
+            price1 = v.select_one('li > p > a > strong').text.strip()
         price1 = int(price1.replace(',',''))
         price0 = round(price1, -(int(math.log10(price1+60000)))) #TODO 올림으로 처리하고 싶은데 방법이 없어 보인다 지금은 반올림(반올림시 할인가격이 더 비싸는 현상 발생) -> 소수점이하로 보낸다음 올림해서 다시 정수로 가져오자!
         #판매자명(cpu와같이 몰이름이 아닌 사양일경우 그값을 가져오자!)
-        mall = v.select_one('li.rank_one > div >p.memory_sect').text.strip()
+        try:
+            mall = v.select_one('li.rank_one > div >p.memory_sect').text.strip()
+        except Exception as e:
+            mall = v.select_one('p.memory_sect').text.strip()
         #스팩
         spec_list = v.select_one('div.spec_list').text.strip()
         #등록일
